261dbb8e0ace005e876577e29a11d3a96372f65f
J05H14 <j05h14@yahoo.com>
2017-09-15 22:02:07 -0700

    started request summary lab

---
+++ src/requestSummary/RequestSummary.java
@@ -0,0 +1,29 @@
+package requestSummary;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.ServletRequest;
+
+/**
+ * Servlet implementation class RequestSummary
+ */
+@WebServlet("/RequestSummary")
+public class RequestSummary extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}

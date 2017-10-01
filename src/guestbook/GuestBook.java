10f48a179dbeb4c78fc775f09d37fbef9bda04ad
J05H14 <j05h14@yahoo.com>
2017-10-01 14:35:22 -0700

    login and cookie is made

--- src/guestbook/GuestBook.java
+++ src/guestbook/GuestBook.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(urlPatterns = "/Labs/GuestBook", loadOnStartup = 2)
+@WebServlet(urlPatterns = "/GuestBook", loadOnStartup = 2)
 public class GuestBook extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
---
+++ src/sessions/LogoutSessions.java
@@ -0,0 +1,36 @@
+package sessions;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/sessions/Logout")
+public class LogoutSessions extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getSession().invalidate();
+		
+		Cookie[] cookies = request.getCookies();
+	    if (cookies != null) {
+	        for (Cookie cookie : cookies) {
+	            cookie.setValue("");
+	            cookie.setPath("/");
+	            cookie.setMaxAge(0);
+	            response.addCookie(cookie);
+	        }
+	    }
+		
+		response.sendRedirect("Login");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
